d6359e305b42652beb775402e3f60975f41e15da
lukaslevickis <levickis93@gmail.com>
2022-04-20 10:37:23 +0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Form from './Form';
+import WindowTracker from './WindowTracker';
 
 function App() {
+  const [show, setShow] = React.useState(true);
+
+  function toggle() {
+    setShow(prevShow => !prevShow)
+  }
+
   return (
     <div className="App">
-      <Form />
+      {/* <Form /> */}
+      <div className='container'>
+        <button onClick={toggle}>toggle</button>
+      </div>
+      {show && <WindowTracker />}
     </div>
   );
 }
